85ef5169bfb5a351d922eb6f217e5933213a0562
Clayton Roth <croth0627@yahoo.com>
2017-06-12 23:03:12 -0700

    added XML filter contracts

---
+++ Bogosoft.Mvc.Xsl/IXmlFilter.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Bogosoft.Mvc.Xsl
+{
+    /// <summary>
+    /// Indicates that an implementation is capable of filtering XML nodes.
+    /// </summary>
+    public interface IXmlFilter
+    {
+        /// <summary>
+        /// Filter a given node and its descendants if any.
+        /// </summary>
+        /// <param name="node">A node to begin filtering on.</param>
+        /// <param name="token">A <see cref="CancellationToken"/> object.</param>
+        /// <returns>
+        /// A <see cref="Task"/> representing the asynchronous operation.
+        /// </returns>
+        Task FilterAsync(XmlNode node, CancellationToken token);
+    }
+}
---
+++ Bogosoft.Mvc.Xsl/XmlFilterAsync.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Bogosoft.Mvc.Xsl
+{
+    /// <summary>
+    /// Filter a given node and its descendants if any.
+    /// </summary>
+    /// <param name="node">A node to begin filtering on.</param>
+    /// <param name="token">A <see cref="CancellationToken"/> object.</param>
+    /// <returns>
+    /// A <see cref="Task"/> representing the asynchronous operation.
+    /// </returns>
+    public delegate Task XmlFilterAsync(XmlNode node, CancellationToken token);
+}
